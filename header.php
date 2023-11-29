c3801ec519576561eef35222dcb5c7cc493f0310
manaswiluitel <manaswiluitel@gmail.com>
2023-11-29 09:01:22 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+ <body>
+    <a href="users.php">
+        <button>Users</button>
+    </a>
+    <a href="events.php">
+        <button>Events</button>
+    </a>
+    <a href="documents.php">
+        <button>Documents</button>
+    </a>
+    <a href="programs.php">
+        <button>Programs</button>
+    </a>
+
+ </body>
+ </html>
