5e0c3326bd98e1fcca2dba717b02a2437be025e7
joshuamd21 <joshua.downey21@gmail.com>
2023-12-03 19:20:33 -0600

    finished the last query for program information management
    finished up the delete query for program information management.
    you can now full delete a program which deletes it along with any information of it in other tables.
    
    you can also just remove access to it which removes all the rows pertaining to it from the Track table so that students are no longer able to access it.

--- deleteProgram.php
+++ deleteProgram.php
@@ -2,16 +2,82 @@
     include_once 'includes/startSession.php';
     include_once "includes/DbConnect.php";
 
-    $sql = "DELETE FROM Programs
-    WHERE Program_Num = " . $_POST["program-id"] . ";";
+    if($_POST['full-delete']){
+        // deleting from Event
+        $sql = "DELETE FROM Event
+        WHERE Program_Num = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
 
-    $result = mysqli_query($conn, $sql);
+        // deleting Documents
+        // selecting applications fo find documents
+        $sql = "SELECT * FROM Application
+        WHERE Program_Num = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        if($resultCheck > 0){
+            while($row = mysqli_fetch_assoc($result)){
+                // selecting documents peratining to this application
+                $sql = "SELECT * FROM Documentation
+                WHERE App_Num = " . $row['App_Num'] . ";";
+                $resultDoc = mysqli_query($conn, $sql);
+                $resultCheck = mysqli_num_rows($result);
+                
+                if($resultCheck > 0){
+                    while($rowDoc = mysqli_fetch_assoc($resultDoc)){
+                        // unlinking the documents we are about to delete
+                        unlink($rowDoc['Link']);
+                    }
+                }
 
-    $keyName = "deletedProgram";
-    if($result){
-        $_SESSION[$keyName] = true;
+                // deleting the documents
+                $sql = "DELETE FROM Documentation
+                WHERE App_Num = " . $row['App_Num'] . ";";
+                $resultDoc = mysqli_query($conn, $sql);
+            }
+            
+        }
+        
+        // deleting from Application
+        $sql = "DELETE FROM Application
+        WHERE Program_Num = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
+
+        // deleting from Track
+        $sql = "DELETE FROM Track
+        WHERE Program = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
+
+        // deleting from Cert_Enrollment
+        $sql = "DELETE FROM Cert_Enrollment
+        WHERE Program_Num = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
+
+        // deleting from programs table
+        $sql = "DELETE FROM Programs
+        WHERE Program_Num = " . $_POST["program-id"] . ";";
+        $result = mysqli_query($conn, $sql);
+
+        $keyName = "deletedProgram";
+        if($result){
+            $_SESSION[$keyName] = true;
+        }
+        else{
+            $_SESSION[$keyName] = false;
+        }
     }
     else{
-        $_SESSION[$keyName] = false;
+        // deleting from track table to remove access to program from students
+        $sql = "DELETE FROM Track
+        WHERE Program = " . $_POST["program-id"] . ";";
+
+        $result = mysqli_query($conn, $sql);
+
+        $keyName = "deletedProgram";
+        if($result){
+            $_SESSION[$keyName] = true;
+        }
+        else{
+            $_SESSION[$keyName] = false;
+        }
     }
     header("Location: programInfo.php");
