4da0d9a07556cf036601af782d2b5e8e291de829
Julia Teleki <juliateleki@tamu.edu>
2023-12-06 11:53:43 -0600

    fix optional connect params

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -4,22 +4,21 @@
     $password = "";
     $database = "id21548664_csce310";
 
+    // keep different settings in a separate DbParams.php file
+    // this way we don't all have to have the same settings
+    if (file_exists('includes/DbParams.php')) {
+        include_once 'includes/DbParams.php';
+    } else {
+        echo "DbParams.php is missing.";
+    }
+
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $database);
 
-// keep different settings in a separate DbParams.php file
-// this way we don't all have to have the same settings
-if (file_exists('includes/DbParams.php')) {
-    include_once 'includes/DbParams.php';
-} else {
-    echo "DbParams.php is missing.";
-}
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
     echo "Connected successfully<br>";
+?>
