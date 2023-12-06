36240bd5c2da1cbf6a420f6ec00e28b57111c00c
Julia Teleki <juliateleki@tamu.edu>
2023-12-06 11:18:22 -0600

    option to keep db credentials separate

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -7,6 +7,14 @@ $username = "root";
 $password = "";
 $database = "id21548664_csce310";
 
+// keep different settings in a separate DbParams.php file
+// this way we don't all have to have the same settings
+if (file_exists('includes/DbParams.php')) {
+    include_once 'includes/DbParams.php';
+} else {
+    echo "DbParams.php is missing.";
+}
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 
